13cca978f347477157c8d0b5bb3a2952bf52aedb
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-02-12 16:16:28 +0700

    update shopping cart manager

---
+++ src/controller/ShoppingCartManager.java
@@ -0,0 +1,58 @@
+package controller;
+
+import model.CartInfo;
+import model.Product;
+
+import java.io.*;
+import java.util.List;
+
+public class ShoppingCartManager extends ApplicationManager<CartInfo>
+{
+    public ShoppingCartManager(String pathDataFile)
+    {
+        super(pathDataFile);
+    }
+
+    @Override
+    public List<CartInfo> readFile()
+    {
+        try(InputStream cartDataFile = new FileInputStream(getPathDataFile());
+            ObjectInputStream objectInputStream = new ObjectInputStream(cartDataFile) )
+        {
+            List<CartInfo> cartInfoList = (List<CartInfo>)objectInputStream.readObject();
+            return cartInfoList;
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Không tìm thấy đường dẫn file !");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Không tìm thấy lớp hợp lệ!");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Lỗi đọc file dữ liệu !");
+        }
+        return null;
+    }
+
+    @Override
+    public void writeFile(List<CartInfo> cartInfoList)
+    {
+        try(OutputStream cartFile = new FileOutputStream(getPathDataFile());
+            ObjectOutputStream objectOutStream = new ObjectOutputStream(cartFile))
+        {
+            objectOutStream.writeObject(cartInfoList);
+            System.out.println("Ghi thành công ! ");
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Không tìm thấy đường dẫn file !");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Lỗi đọc file dữ liệu !");
+        }
+    }
+}
