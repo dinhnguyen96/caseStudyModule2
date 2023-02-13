6f3c9449eb4e26108a64b315eb949c7c6f190715
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-02-13 15:01:45 +0700

    update employee.dat file

---
+++ src/views/Client.java
@@ -0,0 +1,24 @@
+package views;
+
+import controller.ApplicationManager;
+import controller.EmployeeManager;
+import model.Employee;
+import model.Roles;
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+//        ApplicationManager<Categories> categoriesManager = new CategoriesManager("src/storage/file/categories.dat");
+//        ApplicationManager<Product> productManager = new ProductManager("src/storage/file/product.dat");
+//        ApplicationManager<Employee> employeeManager = new EmployeeManager("src/storage/file/employee.dat");
+//        Employee employee = new Employee(1L,"15NV110321","Nguyễn Đình Nguyên","25-10-1996",
+//                                      "Nghệ An", "nguyendinhnguyen2510@gmail.com",
+//                       new User("nguyendinhnguyen","dinhnguyen96",));
+
+    }
+}
