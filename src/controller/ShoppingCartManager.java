c295cb272ef52b0700befb80c40e81562951c2c2
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-02-14 13:58:53 +0700

    add add update delete shopping cart

--- src/controller/ShoppingCartManager.java
+++ src/controller/ShoppingCartManager.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.CartInfo;
+import model.Item;
 import storage.ShoppingCartReadWrite;
 
 import java.util.List;
@@ -27,12 +28,15 @@ public class ShoppingCartManager implements ApplicationManager<CartInfo>
         shoppingCartReadWrite.writeFile(cartInfoList);
     }
 
+
+    // lay cartInfo theo user
+
     @Override
-    public CartInfo get(String code)
+    public CartInfo get(String customerCode)
     {
         for (CartInfo cartInfo:readFile())
         {
-            if (cartInfo.getCustomer().getCustomerCode().equalsIgnoreCase(code))
+            if (cartInfo.getCustomer().getCustomerCode().equalsIgnoreCase(customerCode))
             {
                 return cartInfo;
             }
@@ -40,19 +44,22 @@ public class ShoppingCartManager implements ApplicationManager<CartInfo>
         return null;
     }
 
+    // them gio hang theo user
     @Override
     public boolean add(CartInfo cartInfo)
     {
         CartInfo cart = get(cartInfo.getCustomer().getCustomerCode());
         if (cart == null)
         {
-            cart = new CartInfo();
+            cart = new CartInfo(cartInfo.getCustomer());
         }
         List<CartInfo> updateCartInfoList = readFile();
         updateCartInfoList.add(cart);
         writeFile(updateCartInfoList);
         return true;
     }
+
+    // cap nhat gio hang theo user
     @Override
     public boolean update(CartInfo cartInfo)
     {
@@ -67,6 +74,8 @@ public class ShoppingCartManager implements ApplicationManager<CartInfo>
         return false;
     }
 
+
+    // xoa  gio hang theo user
     @Override
     public boolean remove(CartInfo cartInfo)
     {
@@ -80,4 +89,64 @@ public class ShoppingCartManager implements ApplicationManager<CartInfo>
         }
         return false;
     }
+
+
+    // lay items in cart of user
+    public Item getItem(String customerCode, Item item)
+    {
+        CartInfo cartInfo = get(customerCode);
+
+        for (Item i: cartInfo.getCartList())
+        {
+             if (i.getProduct().getProductCode().equalsIgnoreCase(item.getProduct().getProductCode()))
+             {
+                 return i;
+             }
+        }
+        return null;
+    }
+
+    // add item in cart of user
+    public void addItem(String customerCode,Item item)
+    {
+        Item i = getItem(customerCode, item);
+
+        CartInfo cartInfo = get(customerCode);
+
+        if (i == null)
+        {
+            i = new Item();
+            i.setQuantity(1);
+            cartInfo.getCartList().add(i);
+        }
+        else
+        {
+            i.setQuantity(i.getQuantity()+1);
+        }
+        cartInfoList.set(cartInfoList.indexOf(cartInfo), cartInfo);
+        writeFile(cartInfoList);
+    }
+    // add item in cart of user
+    public void removeItem(String customerCode, Item item)
+    {
+        Item i = getItem(customerCode, item);
+
+        CartInfo cartInfo = get(customerCode);
+
+        if (i != null)
+        {
+            cartInfo.getCartList().remove(i);
+        }
+        cartInfoList.set(cartInfoList.indexOf(cartInfo), cartInfo);
+        writeFile(cartInfoList);
+    }
+    // clear item in cart of user
+    public void removeAllItem(String customerCode)
+    {
+        CartInfo cartInfo = get(customerCode);
+        cartInfo.getCartList().clear();
+        cartInfoList.set(cartInfoList.indexOf(cartInfo), cartInfo);
+        writeFile(cartInfoList);
+    }
+
 }
